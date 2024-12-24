d2ae746bb5bc56db7438147941a21244de0b837f
odenix <self@odenix.org>
2024-12-24 14:59:41 -0800

    Add module descriptor

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * A Java URI library compliant with RFC 3986.
+ */
+// Suppress the following warning, which is meant to discourage including version information in module name:
+// warning: [module] module name component rfc3986 should avoid terminal digits
+@SuppressWarnings({"module", "JavaModuleNaming"})
+module org.czeal.rfc3986 {
+  exports org.czeal.rfc3986;
+}
